d80fa49ec7023c0a47326636811bce7a2fbfaae0
Eliot Pearson Jr <eliotnumber2@gmail.com>
2025-05-03 16:24:22 -0400

    implemnted new ui for landing page, with 3 stamp buttons

--- app/page.tsx
+++ app/page.tsx
@@ -7,30 +7,62 @@ export default function Home() {
   return (
     <div className="postcard-bg">
       <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-        <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+        <main className="flex gap-[50px] row-start-2 items-center sm:items-start">
 
-          <Image 
-          // className=
-            src="/Kitsune_Postcard.png"
-            width={200}
-            height={200}
-            alt="Kistune postcard test image"
-            // priority
-          />
+        <a
+            //className="hover:bg-[#383838] dark:hover:bg-[#ccc]"
+            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image 
+              // className=
+                src="/Sprites/Stamp_Web_Button_Work_Slant.png"
+                width={300}
+                height={300}
+                alt="Links Info Stamp image"
+                // priority
+              />
+        </a>
+
+        <a
+            //className="hover:bg-[#383838] dark:hover:bg-[#ccc]"
+            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image 
+              // className=
+                src="/Sprites/Stamp_Web_Button_Links_Slant.png"
+                width={300}
+                height={300}
+                alt="Links Info Stamp image"
+                // priority
+              />
+          </a>
+          
+          <a
+            //className="hover:bg-[#383838] dark:hover:bg-[#ccc]"
+            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image 
+              // className=
+                src="/Sprites/Stamp_Web_Button_Contact_Slant.png"
+                width={300}
+                height={300}
+                alt="Contact Info Stamp image"
+                // priority
+              />
+          </a>
+
+          
+          
+            
 
 
-          <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-            <li className="mb-2 tracking-[-.01em]">
-              Get started by editing{" "}
-              <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-                app/page.tsx
-              </code>
-              .
-            </li>
-            <li className="tracking-[-.01em]">
-              Save and see your changes instantly.
-            </li>
-          </ol>
+          
 
           <div className="flex gap-4 items-center flex-col sm:flex-row">
             <a
@@ -48,14 +80,7 @@ export default function Home() {
               />
               Deploy now
             </a>
-            <a
-              className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-              href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Read our docs
-            </a>
+            
           </div>
         </main>
         <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
