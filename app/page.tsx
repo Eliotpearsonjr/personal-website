6cfaf8662749a7708bf03477fa48b2191480b914
Eliot Pearson Jr <eliotnumber2@gmail.com>
2025-05-02 23:53:45 -0400

    got rid of default logo

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,21 @@
 import Image from "next/image";
+// Personal Portfolio Website
+// Eliot Pearson Jr
+// Developement Started: 5/2/25
 
 export default function Home() {
   return (
     <div className="postcard-bg">
       <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
         <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-          <Image
-            className="dark:invert"
-            src="/next.svg"
-            alt="Next.js logo"
-            width={180}
-            height={38}
-            priority
-          />
 
-          <Image src="/Kitsune_Postcard.png"
+          <Image 
+          // className=
+            src="/Kitsune_Postcard.png"
             width={200}
             height={200}
             alt="Kistune postcard test image"
+            // priority
           />
 
 
