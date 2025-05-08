2871892bffb24ca7f68eeff2784d41f9504ab5a2
Eliot Pearson <eliotp1@umbc.edu>
2025-05-08 00:22:24 -0400

    began adding conact information to the contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -7,7 +7,14 @@ export default function Contact() {
     return (
         <div className="postcard-bg">
             <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-                <p>This is the contact page</p>
+                
+                <p>Feel free to get in touch with me check out my social medias!</p>
+
+                <p>eliotnumber2@gmail.com</p>
+                <a>LinkedIn</a>
+                <a>Instagram</a>
+                <a>Github</a>
+
             </div>
         </div>
     );
