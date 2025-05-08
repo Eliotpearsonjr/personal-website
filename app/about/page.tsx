697637d5368627ed141bd8f20a9fc9584fa39a99
Eliot Pearson <eliotp1@umbc.edu>
2025-05-07 22:58:36 -0400

    added the about page to the site with functional button

---
+++ app/about/page.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+// Personal Portfolio Website
+// About Page
+// Eliot Pearson Jr
+// Developement Started: 5/2/25
+export default function About() {
+    return (
+        <div className="postcard-bg">
+            <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+                <p>This is the about page</p>
+            </div>
+        </div>
+    );
+}
