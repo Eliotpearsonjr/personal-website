a164922eaac289e2ecee0681524811be969174a7
Eliot Pearson <eliotp1@umbc.edu>
2025-05-07 22:50:13 -0400

    added the contact page to the site, contact button is functional

---
+++ app/contact/page.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+// Personal Portfolio Website
+// Contact Page
+// Eliot Pearson Jr
+// Developement Started: 5/2/25
+export default function Contact() {
+    return (
+        <div className="postcard-bg">
+            <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+                <p>This is the contact page</p>
+            </div>
+        </div>
+    );
+}
