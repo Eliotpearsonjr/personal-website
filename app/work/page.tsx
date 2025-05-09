99fa79a6baabc46a93db9a27915039b627df8f24
Eliot Pearson Jr <eliotnumber2@gmail.com>
2025-05-08 23:32:43 -0400

    added a page and the first icons for the work page, as well as subpages for these projects

--- app/work/page.tsx
+++ app/work/page.tsx
@@ -6,8 +6,49 @@ import Image from "next/image";
 export default function Contact() {
     return (
         <div className="postcard-bg">
-            <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-                <p>This is the work page</p>
+            <div className="grid grid-rows-[20px_1fr_200px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+                <p>Choose a project.</p>
+                <a
+                    //className="hover:bg-[#383838] dark:hover:bg-[#ccc]"
+                    href="/project_pages/plip_pets"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    >
+                    <Image
+                        src="/Sprites/Plip_Pets_Logo_Sticker_Transparent_.png"
+                        width={500}
+                        height={300}
+                        alt="Plip Pets Logo Button"
+                        />
+                </a>
+
+                <a
+                    //className="hover:bg-[#383838] dark:hover:bg-[#ccc]"
+                    href="/project_pages/plip_pets"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    >
+                    <Image
+                        src="/Sprites/Beer_N_Bots_Logo_Sticker.png"
+                        width={600}
+                        height={300}
+                        alt="Beer N Bots Logo Button"
+                        />
+                </a>
+
+                <a
+                    //className="hover:bg-[#383838] dark:hover:bg-[#ccc]"
+                    href="/project_pages/plip_pets"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    >
+                    <Image
+                        src="/Sprites/Reward_Sprout_Big_Icon.png"
+                        width={300}
+                        height={300}
+                        alt="Reward Sprout Logo Button"
+                        />
+                </a>
             </div>
         </div>
     );
