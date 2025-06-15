951a21ccae3ae23ecc7f007456abd1d054bbffa1
Eliot Pearson Jr <eliotnumber2@gmail.com>
2025-06-14 20:17:32 -0400

    Started adding things to the about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -6,8 +6,17 @@ import Image from "next/image";
 export default function About() {
     return (
         <div className="bg-orange-100 p-1 mt-10 ml-10 mr-10 mb-10 rounded-lg">
-            <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-                <p>This is the about page</p>
+            <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 gap-2 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+                <div className="bg-orange-900/5 font-semibold text-lg text-stone-700/90 p-4 pb-8 rounded-md ml-5 mr-5">
+                    <p>Hello! My name is Eliot, and I am a front-end developer and UX Designer.
+                        I am also an illustrator! Ever since I was little I have always loved art
+                        and design, but I was quite interested in computers as well. Becoming a
+                        front-end developer with focus on UI/UX has allowed for my to bridge the
+                        gap between my passions, which is why in made this website. This site serves
+                        as a place for all of the projects, applications, and game that I have worked
+                         on, so please enjoy!
+                    </p>
+                </div>
             </div>
         </div>
     );
--- app/page.tsx
+++ app/page.tsx
@@ -8,18 +8,24 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="bg-orange-100 p-1 mt-10 ml-10 mr-10 mb-10 rounded-lg">
-      <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-4 gap-12 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-
+      <div className="grid grid-rows font-semibold text-3xl items-center justify-items-center min-h-screen p-2 gap-10 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+        
         <p>Greetings From,</p>
+
+        <div className="font-bold text-6xl">
+          <p>Eliot Pearson Jr</p>
+        </div>
 
         <main className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-4 sm:p-4 justify-items-center rounded-sm font-[family-name:var(--font-geist-sans)]">
 
         <a
-            className="flex flex-col items-center text-center cursor-pointer transition-transform ease-in-out hover:scale-120 text-center"
+            className="flex flex-col items-center font-semibold text-4xl text-center cursor-pointer transition-transform ease-in-out hover:scale-120 text-center"
             href="/work"
             target="_blank"
             rel="noopener noreferrer"
           >
+
+            <p>Projects</p>
             <Image 
               // className=
                 src="/Sprites/Projects_Sticker_Button.png"
@@ -29,16 +35,16 @@ export default function Home() {
                 // priority
               />
 
-              <p>Projects</p>
         </a>
           
           
           <a
-            className="flex flex-col items-center text-center cursor-pointer transition-transform ease-in-out hover:scale-120 text-center"
+            className="flex flex-col items-center font-semibold text-4xl text-center cursor-pointer transition-transform ease-in-out hover:scale-120 text-center"
             href="/contact"
             target="_blank"
             rel="noopener noreferrer"
           >
+            <p>Contact</p>
             <Image 
                 
                 src="/Sprites/Contact_Sticker_Button.png"
@@ -48,15 +54,16 @@ export default function Home() {
                 // priority
               />
 
-              <p>Contact</p>
+
           </a>
 
           <a
-            className="flex flex-col items-center text-center cursor-pointer transition-transform ease-in-out hover:scale-120 text-center"
+            className="flex flex-col items-center font-semibold text-4xl text-center cursor-pointer transition-transform ease-in-out hover:scale-120 text-center"
             href="/about"
             target="_blank"
             rel="noopener noreferrer"
           >
+            <p>About</p>
             <Image 
               // className=
                 src="/Sprites/About_Sticker_Button.png"
@@ -66,24 +73,10 @@ export default function Home() {
                 // priority
               />
 
-              <p>About</p>
+              
           </a>
-
-          
-          
-            
-
-
-          
-
-          <div className="flex gap-4 items-center flex-col sm:flex-row">
-            
-            
-          </div>
         </main>
-        <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-          
-        </footer>
+        
       </div>
     </div>
     
