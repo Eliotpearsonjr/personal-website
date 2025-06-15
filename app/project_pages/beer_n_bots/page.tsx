abb3e4da6cee4a640530209fc1a81614fc02e527
Eliot Pearson Jr <eliotnumber2@gmail.com>
2025-06-15 16:47:27 -0400

    updated Beer N Bots page with new images and populated proper information

--- app/project_pages/beer_n_bots/page.tsx
+++ app/project_pages/beer_n_bots/page.tsx
@@ -6,107 +6,133 @@ import {useState} from "react";
 // Eliot Pearson Jr
 // Developement Started: 5/2/25
 export default function BeerNBots() {
-    // varibles for selecting plip type descriptions
-    const [selectedPlip, setSelectedPlip] = useState<string | null>(null);
-
-
     return (
         // inner postcard background
         <div className="bg-orange-100 p-1 mt-10 ml-10 mr-10 mb-10 rounded-lg">
 
             {/* text box div, mt-35 makes the top border of the postcard visible from the header */}
-            <div className="flex flex-col gap-2 mt-10">
+            <div className="flex flex-col mt-10">
             
                 <Image
-                    className="mb-4 mx-auto block h-auto"
+                    className="mx-auto block h-auto"
                     src="/Sprites/Beer_N_Bots_Logo_Sticker.webp"
-                    width={650}
+                    width={600}
                     height={300}
                     alt="Beer N Bots Logo Header"
                 />
 
-                {/* Section Title - Plip Pets */}
-                <div className="text-stone-700 p-4 rounded-md ml-10 mr-10">
-                    <p className="text-lg font-semibold">Beer N Bots</p>
+                {/* Section Title - Beer N Bots */}
+                <div className="text-stone-700 border-b-2 border-stone-700/10 pb-2 ml-7 mr-7 mb-2">
+                    <p className="text-2xl font-semibold">Beer N Bots</p>
                 </div>
 
-                {/* Section Body - Summary */}
-                <div className="bg-orange-900/5 text-stone-700/90 p-4 pb-8 rounded-md ml-10 mr-10 mb-2">
-                    <p className="text-lg font-regular">
-                    Beer N Bots is held twice a year, and hosted by the <b>Baltimore Robotics Center.</b> 
-                    I worked with the Robotics Center team to create a new logo, as well as promotional 
-                    flyers for the event. The look and feel of the graphics evoke an aesthetic of modern 
-                    industry and connection, channeling the Robotics Center's commitment to community 
-                    outreach and serving as a cornerstone for innovation.
-                    </p>
-                </div>
+                {/* Holds split subsections */}
+                <div className="flex flex-col lg:flex-row gap-8 justify-center items-center">
+                    {/* Section Body - Summary */}
+                    <div className="flex flex-col sm:flex-row gap-4 w-full lg:w-full mb-5">
+                        <p className="bg-orange-50 text-stone-700/90 ml-5 mr-5 p-4 rounded-2xl text-lg font-regular">
+                            Beer N Bots is an anual event hosted by the Baltimore Robotics Center. The 
+                            event allows for representatives from local tech companies to compete in a
+                            cutthroat battle with robots!
+                            I worked with the Robotics Center Team to provide a modern, eye-catching 
+                            and fresh logo for the event, along with a new branding direction for the 
+                            Center's official social media pages that are used to announce competitors. 
+                            I created the designs using <b>Proceate.</b>              
+                        </p>
+                    </div>
+                
 
-
-                {/* Section Title - Gallery */}
-                <div className="text-stone-700 p-4 rounded-md ml-10 mr-10">
-                    <p className="text-lg font-semibold">Social Media Flyers</p>
-                </div>
-
-                {/* Concept Art Images */}
-                <div className="bg-neutral-50 mt-4 mb-4 mr-8 ml-8 pb-20 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 p-4 sm:p-10 justify-items-center rounded-sm font-[family-name:var(--font-geist-sans)]">
-                    
-                    <a
-                        className="transition-transform ease-in-out hover:scale-110"
-                        href="/Sprites/BNB_Christina_Krueger_SOCIAL.png"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        <Image
-                            
-                            src="/Sprites/BNB_Christina_Krueger_SOCIAL.png"
-                            width={300}
-                            height={300}
-                            alt="Beta Home Screen Image"
-                        />
-                    </a>
-
-                    <a
-                        className="transition-transform ease-in-out hover:scale-110"
-                        href="/Sprites/BNB_Margaret_Hart_SOCIAL.png"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        <Image
-                            src="/Sprites/BNB_Margaret_Hart_SOCIAL.png"
-                            width={300}
-                            height={300}
-                            alt="Beta Fridge Image"
-                        />
-                    </a>
-
-
-                </div>   
-
-                {/* Section Title - Plip Pets */}
-                <div className="bg-orange-900/5 text-stone-700/90 p-4 pb-4 rounded-md ml-10 mr-10 mb-5 text-center">
-                    <p className="text-lg font-semibold">Building Futures, Brewing Innovation</p>
-                </div>
-
-                {/* Section Title - Tools Used */}
-                <div className="text-stone-700 p-4 rounded-md ml-10 mr-10">
-                    <p className="text-lg font-semibold">Tools Used</p>
-                </div>
-
-                {/* Stamps with logos of software products used */}
-                <div className= "grid grid-row sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 sm:p-2 justify-items-left">
-
-                    <a className="text-left">
-                        <Image
-                                    
-                            src="/Sprites/Procreate_Stamp.png"
-                            width={250}
-                            height={250}
-                            alt="Procreate Logo Stamp"
-                        />
-                        <p className="text-lg font-semibold">Procreate</p>
-                    </a>
+                    {/* Stamps with logos of software products used */}
+                    <div className= "flex flex-col lg:flex-row ml-5 mb-5">
+                        <a className="text-center">
+                            <Image
+                                className="rotate-5"       
+                                src="/Sprites/Procreate_Stamp.webp"
+                                width={250}
+                                height={250}
+                                alt="Procreate Logo Stamp"
+                            />
+                            <p className="text-lg font-semibold">Procreate</p>
+                        </a>
+                    </div>
                 </div>
                 
+                {/* Section Title - Promotional Materials Gallery */}
+                <div className="text-stone-700 border-b-2 border-stone-700/10 pb-2 ml-7 mr-7 mb-2">
+                    <p className="text-2xl font-semibold">Promotional Materials</p>
+                </div>
+
+                {/* Promotional Images - horizontal scrollable gallery */}
+                <div className="bg-stone-50/90 mt-4 mb-4 mx-5 pt-8 pb-8 px-8 rounded-xl overflow-x-auto">
+                    <div className="flex space-x-6 items-center justify-start min-w-fit w-max">
+
+                        <a
+                            className="transition-transform ease-in-out hover:scale-110"
+                            href="/Sprites/BNB_Christina_Krueger_SOCIAL.png"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            <Image
+                                className="rounded-xl drop-shadow-lg"
+                                src="/Sprites/BNB_Christina_Krueger_SOCIAL.png"
+                                width={300}
+                                height={300}
+                                alt="Beer N Bots Social Media Post 1"
+                            />
+                        </a>
+
+                        <a
+                            className="transition-transform ease-in-out hover:scale-110"
+                            href="/Sprites/BNB_Margaret_Hart_SOCIAL.png"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            <Image
+                                className="rounded-xl drop-shadow-lg"
+                                src="/Sprites/BNB_Margaret_Hart_SOCIAL.png"
+                                width={300}
+                                height={300}
+                                alt="Beer N Bots Social Media Post 2"
+                            />
+                        </a>
+
+                        <a
+                            className="transition-transform ease-in-out hover:scale-110"
+                            href="/Sprites/Beer_N_Bots_Color_Palette.png"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            <Image
+                                className="rounded-xl drop-shadow-lg"
+                                src="/Sprites/Beer_N_Bots_Color_Palette.png"
+                                width={300}
+                                height={300}
+                                alt="Official Color Palette for Logo"
+                            />
+                        </a>
+
+                        <a
+                            className="transition-transform ease-in-out hover:scale-110"
+                            href="/Sprites/Beer_N_Bots_Mascot.png"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            <Image
+                                className="rounded-xl drop-shadow-lg"
+                                src="/Sprites/Beer_N_Bots_Mascot.png"
+                                width={300}
+                                height={300}
+                                alt="Beer N Bots Mascot Image"
+                            />
+                        </a>
+                    </div>
+                </div>
+
+                {/* Section Title - Slogan Footer */}
+                <div className="text-stone-700 border-t-2 border-stone-700/10 py-2 ml-7 mr-7 mt-4 mb-2">
+                    <p className="text-2xl font-semibold">Brewing Innovation, Building Futures</p>
+                </div>
+
             </div>
             
         </div>
