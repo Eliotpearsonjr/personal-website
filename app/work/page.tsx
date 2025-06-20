0ae4d8721b20f4df617bf2f1f48a6e318d6ef0a9
Eliot Pearson <eliotp1@umbc.edu>
2025-06-20 10:46:46 -0400

    added dark mode to projects page, added mix n munch beta icon

--- app/work/page.tsx
+++ app/work/page.tsx
@@ -5,9 +5,16 @@ import Image from "next/image";
 // Developement Started: 5/2/25
 export default function Contact() {
     return (
-        <div className="bg-orange-100 p-1 mt-10 ml-10 mr-10 mb-10 rounded-lg">
-            <div className="grid grid-rows-[20px_1fr_200px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-                <p>Choose a project.</p>
+        // inner postcard background
+        <div className="bg-orange-100 dark:bg-stone-800 text-stone-700 dark:text-stone-100 p-1 mt-10 ml-10 mr-10 mb-10 rounded-lg">
+
+            <div className="justify-items-center items-center bg-orange-50 dark:bg-stone-800 text-stone-800 dark:text-stone-100 mx-10 mt-10 p-4 rounded-2xl text-left border-2 border-orange-900/30 dark:border-stone-600 transition-opacity duration-300 text-lg font-regular">
+                <p>Choose a project!</p>
+            </div>
+            
+            {/* list of projects */}
+            <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+                
                 <a
                     className="rotate-350 transition-transform ease-in-out hover:scale-120"
                     href="/project_pages/plip_pets"
@@ -47,9 +54,24 @@ export default function Contact() {
                     <Image
                         className="drop-shadow-lg"
                         src="/Sprites/Reward_Sprout_Big_Icon.webp"
-                        width={300}
-                        height={300}
+                        width={200}
+                        height={200}
                         alt="Reward Sprout Logo Button"
+                        />
+                </a>
+
+                <a
+                    className="transition-transform ease-in-out hover:scale-120"
+                    href="/project_pages/plip_pets"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    >
+                    <Image
+                        className="drop-shadow-lg"
+                        src="/Sprites/Mix_N_Munch_App_Icon.png"
+                        width={200}
+                        height={200}
+                        alt="Mix N Munch Logo Button"
                         />
                 </a>
             </div>
